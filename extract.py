9a48f5b265e541a17e8b73183fe685d1e85b4567
EdenTP <edenthiedepalmer@gmail.com>
2026-07-08 15:47:47 +0100

    error_handling

--- extract.py
+++ extract.py
@@ -6,8 +6,9 @@ import logging
 import time
 
 logging.basicConfig(level=logging.INFO)
+os.makedirs('data', exist_ok=True)
+
 url = 'https://api.tfl.gov.uk/BikePoint/'
-data_dir = os.makedirs('data', exist_ok=True)
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 file_name = f'data/bike_points_{timestamp}.json'
 max_retry = 5
@@ -15,10 +16,26 @@ attempt = 0
 delay = 10
 
 #while attempt < max_retry AND 
-response = req.get(url)
-data = response.json()
 
-with open(file_name,'w') as file:
-    json.dump(data,file)
+
+while attempt<=max_retry :
+    response = req.get(url)
+    data = response.json()
+    status=response.status_code
+    
+    if 200 <= status < 300:
+        with open(file_name,'w') as file:
+            json.dump(data,file)
+            print('suffering from success')
+            break
+    elif status <= 100 or status>=500:
+        attempt=attempt+1
+        print(f'Sus status waiting to try again, attempt number {attempt}')
+        time.sleep(delay)
+    else : 
+        print('panic')
+        print(response.status_code)
+        break
+
 
 
