5cdd3b37e81d8c063e0e5eee0815bce896dba595
EdenTP <edenthiedepalmer@gmail.com>
2026-07-14 15:43:13 +0100

    code modularised incl. logging (#5)

---
+++ modules/log_function.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+def log_config(timestamp:str,log_dir:str):
+    """This initialises logging
+
+    Args:
+        timestamp (str): For the filename of the logs
+        log_dir (str): Where we want the logs stored
+    
+    
+    """
+
+    os.makedirs(log_dir, exist_ok=True)
+    log_filename = f'{log_dir}/{timestamp}.log'
+
+    handlers=[
+        logging.FileHandler(f"{log_filename}"),  # Saves to file
+        logging.StreamHandler()                        # Prints to terminal
+    ]
+    logging.basicConfig(
+        format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO,
+        force=True,
+        handlers=handlers
+    )
+
+    logger = logging.getLogger()
+    return logger
